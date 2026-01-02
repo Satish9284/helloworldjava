38ac32f1ba4fcfa7fc3dbb90c79d140945f716c5
Satish Sonone <125435030+Satish9284@users.noreply.github.com>
2026-01-02 13:30:10 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - jan 2nd 2026 - 13:04 PM - poll scm cron/job ");
+    System.out.println("Hello World - jan 2nd 2026 - 13:30 PM - poll scm cron/job ");
     }
 }
